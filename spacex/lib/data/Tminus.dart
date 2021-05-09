eceb26037f7c5fda3328a150857b03a22749850d
TNelen <timo.nelen@gmail.com>
2021-05-09 09:56:36 +0200

    version 1.0.4 time bugfixes

--- spacex/lib/data/Tminus.dart
+++ spacex/lib/data/Tminus.dart
@@ -4,19 +4,30 @@ class Tminus {
   Tminus();
 
   String calculateTminus(String liftoff) {
-    DateTime launchtime = DateTime.parse(liftoff).toUtc();
+    DateTime launchtime = DateTime.parse(liftoff);
     DateTime now = DateTime.now().toUtc();
 
-    Duration difference = launchtime.difference(now);
-    return prettyDuration(difference, abbreviated: true, delimiter: ": ");
+    print(launchtime);
+    print(launchtime.toUtc());
+
+    if (launchtime.isAfter(now)) {
+      Duration difference = launchtime.difference(now);
+      return prettyDuration(difference, abbreviated: true, delimiter: ": ");
+    } else {
+      return "0";
+    }
   }
 
   String calculateTminusNoSec(String liftoff) {
     DateTime launchtime = DateTime.parse(liftoff).toUtc();
     DateTime now = DateTime.now().toUtc();
 
-    Duration difference = launchtime.difference(now);
-    return prettyDuration(difference,
-        abbreviated: true, delimiter: ": ", tersity: DurationTersity.minute);
+    if (launchtime.isAfter(now)) {
+      Duration difference = launchtime.difference(now);
+      return prettyDuration(difference,
+          abbreviated: true, delimiter: ": ", tersity: DurationTersity.minute);
+    } else {
+      return "0";
+    }
   }
 }
